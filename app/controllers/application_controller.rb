95f5270007b8c38d5433076b39b73d6ef7b7c88e
cjbutcher <christopherbutcher@live.com>
2016-12-30 00:04:55 +0000

    Bug fixes and bit of styling on sign up page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def reject_request!
     raise "User is not authorized"
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
 end
